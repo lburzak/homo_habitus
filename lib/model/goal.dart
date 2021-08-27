cd5b1fb2ce6dce900a9c6523d39dda6e74855616
polydome <polydome@outlook.com>
2021-08-27 12:48:32 +0200

    Add `Goal.progressPercentage` property getter

--- lib/model/goal.dart
+++ lib/model/goal.dart
@@ -4,6 +4,8 @@ class Goal {
   GoalUnit unit;
 
   Goal({required this.current, required this.target, required this.unit});
+
+  double get progressPercentage => current / target;
 }
 
 enum GoalUnit {
