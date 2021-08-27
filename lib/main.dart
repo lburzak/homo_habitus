c3b0d89f989b2f445ba8bdccf470f1b96b0ead3b
polydome <polydome@outlook.com>
2021-08-27 11:36:02 +0200

    Create `SectionLabel` widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,31 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Homo Habitus',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+          primarySwatch: Colors.green,
+          textTheme: TextTheme(
+              headline6: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w300, fontSize: 24),
+              subtitle1: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold, fontSize: 18),
+          )
       ),
-      home: const Scaffold(),
+      home: const Scaffold(
+        body: SectionLabel(),
+      ),
     );
   }
+}
+
+class SectionLabel extends StatelessWidget {
+  const SectionLabel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Today", style: Theme.of(context).textTheme.headline6),
+          Text("47%", style: Theme.of(context).textTheme.subtitle1,)
+        ],
+      );
 }
