24e7a3803f91531660a8d1f7391019c5f96e52fc
polydome <polydome@outlook.com>
2021-08-27 11:43:55 +0200

    Add padding

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,33 @@ class MyApp extends StatelessWidget {
           textTheme: TextTheme(
               headline6: GoogleFonts.poppins(
                   fontWeight: FontWeight.w300, fontSize: 24),
-              subtitle1: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold, fontSize: 18),
-          )
-      ),
+              subtitle1: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 18),
+          )),
       home: const Scaffold(
-        body: SectionLabel(),
+        body: HabitsPage(),
       ),
     );
   }
+}
+
+class HabitsPage extends StatelessWidget {
+  const HabitsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: SectionLabel(),
+        ),
+      );
 }
 
 class SectionLabel extends StatelessWidget {
   const SectionLabel({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      Row(
+  Widget build(BuildContext context) => Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text("Today", style: Theme.of(context).textTheme.headline6),
