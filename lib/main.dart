d6c29c05559022c4d08d7aee7b3b01d89b6e1e0e
polydome <polydome@outlook.com>
2021-08-27 16:12:53 +0200

    Put section and indicators into a scrollable view

--- lib/main.dart
+++ lib/main.dart
@@ -36,9 +36,32 @@ class HabitsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          child: SectionLabel(),
+        child: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              sliver: SliverFixedExtentList(delegate: SliverChildBuilderDelegate(
+                  (context, index) => SectionLabel(),
+                childCount: 1
+              ), itemExtent: 30),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+              sliver: SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                      (context, index) => Center(
+                        child: RoundIndicator(
+                              height: 78,
+                              width: 78,
+                              progressValue: 0.4,
+                              icon: Icons.book,
+                              onPressed: () {},
+                            ),
+                      ),
+                      childCount: 16),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 12, mainAxisSpacing: 12)),
+            )
+          ],
         ),
       );
 }
@@ -47,22 +70,13 @@ class SectionLabel extends StatelessWidget {
   const SectionLabel({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Column(
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Today", style: Theme.of(context).textTheme.headline6),
-              Text(
-                "47%",
-                style: Theme.of(context).textTheme.subtitle1,
-              )
-            ],
-          ),
-          RoundIndicator(
-            progressValue: 0.4,
-            icon: Icons.fitness_center,
-            onPressed: () {},
+          Text("Today", style: Theme.of(context).textTheme.headline6),
+          Text(
+            "47%",
+            style: Theme.of(context).textTheme.subtitle1,
           )
         ],
       );
