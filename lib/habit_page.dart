873438892747de34b09e33d97cb896daf5cc2b52
polydome <polydome@outlook.com>
2021-08-30 11:15:03 +0200

    Add progress indicator to habit page

--- lib/habit_page.dart
+++ lib/habit_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:homo_habitus/repository/habit_repository.dart';
+import 'package:homo_habitus/round_indicator.dart';
 
 class HabitPage extends StatelessWidget {
   final habitRepository = HabitRepository();
@@ -14,6 +15,33 @@ class HabitPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(habit.name),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              Center(
+                child: RoundIndicator(
+                    height: 180,
+                    width: 180,
+                    progressValue: habit.goal.progressPercentage,
+                    progressStrokeWidth: 8,
+                    icon: IconData(habit.iconCodePoint,
+                        fontFamily: "MaterialIcons")),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 36),
+                child: Text(
+                  "${(habit.goal.progressPercentage * 100).toStringAsFixed(0)}%",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              )
+            ],
+          ),
+
+        ],
       ),
     );
   }
