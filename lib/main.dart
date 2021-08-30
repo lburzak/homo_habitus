9398e903eb4cde0f510a7e66aba44f43a2d36be8
polydome <polydome@outlook.com>
2021-08-30 12:17:24 +0200

    Extract theme declaration to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:homo_habitus/repository/habit_repository.dart';
+import 'package:homo_habitus/theme.dart';
 
 import 'page/habit_page.dart';
 import 'page/home_page.dart';
@@ -20,32 +20,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Homo Habitus',
-        theme: ThemeData(
-            colorScheme: const ColorScheme.dark(
-                onBackground: Color(0xff393939), primary: Color(0xff306F1A)),
-            iconTheme: const IconThemeData(color: Color(0xffBEBEBE)),
-            appBarTheme: AppBarTheme(
-                toolbarHeight: 80,
-                centerTitle: true,
-                elevation: 0,
-                titleTextStyle: Theme.of(context).textTheme.headline6,
-            textTheme: TextTheme(
-              headline6: GoogleFonts.poppins(fontWeight: FontWeight.w300, fontSize: 24),
-            ),
-            backwardsCompatibility: true,
-              backgroundColor: Colors.transparent),
-          textTheme: TextTheme(
-            headline4: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold, fontSize: 36),
-              headline5: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: const Color(0xff7d7d7d)),
-              headline6: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w300, fontSize: 24),
-              subtitle1: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold, fontSize: 18),
-            )),
+        theme: themeData,
         routes: {
           "/": (context) => const HomePage(),
           "/habit": (context) => const HabitPage(),
---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final themeData = ThemeData(
+    colorScheme: const ColorScheme.dark(
+        onBackground: Color(0xff393939), primary: Color(0xff306F1A)),
+    iconTheme: const IconThemeData(color: Color(0xffBEBEBE)),
+    appBarTheme: _appBarTheme,
+    textTheme: _textTheme);
+
+final _textTheme = TextTheme(
+  headline4: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 36),
+  headline5: GoogleFonts.poppins(
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+      color: const Color(0xff7d7d7d)),
+  headline6: GoogleFonts.poppins(fontWeight: FontWeight.w300, fontSize: 24),
+  subtitle1: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 18),
+);
+
+final _appBarTheme = AppBarTheme(
+    toolbarHeight: 80,
+    centerTitle: true,
+    elevation: 0,
+    titleTextStyle: _textTheme.headline6,
+    textTheme: _textTheme,
+    backwardsCompatibility: true,
+    backgroundColor: Colors.transparent);
