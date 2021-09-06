f207dd443e6c082d758dfc140ba73e979f7e37ed
polydome <polydome@outlook.com>
2021-09-06 14:28:32 +0200

    Flatten duration picker hierarchy

--- lib/page/create_habit_page.dart
+++ lib/page/create_habit_page.dart
@@ -259,51 +259,94 @@ class DurationPicker extends StatelessWidget {
           children: [
             Expanded(
                 child: Column(
-              children: [
-                Text("hours",
-                    style: Theme.of(context)
-                        .textTheme
-                        .subtitle1
-                        ?.copyWith(color: const Color(0x99ffffff))),
+              children: const [
+                PickerLabel("hours"),
                 Expanded(
-                  child: NumberPicker.builder(
-                    min: 0,
-                    max: 12,
-                    step: 1,
-                    buildChild: (number) => Text(number.toString(),
-                        style: Theme.of(context).textTheme.subtitle1),
-                  ),
+                  child: HoursPicker(),
                 ),
               ],
             )),
-            SizedBox(
+            const SizedBox(
                 width: 30,
-                child: Text(":",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .subtitle1
-                        ?.copyWith(color: const Color(0x99ffffff)))),
+                child: PickerSeparator()),
             Expanded(
                 child: Column(
-              children: [
-                Text("minutes",
-                    style: Theme.of(context)
-                        .textTheme
-                        .subtitle1
-                        ?.copyWith(color: const Color(0x99ffffff))),
+              children: const [
+                PickerLabel("minutes"),
                 Expanded(
-                  child: NumberPicker.builder(
-                    min: 0,
-                    max: 60,
-                    step: 5,
-                    buildChild: (number) => Text(number.toString(),
-                        style: Theme.of(context).textTheme.subtitle1),
-                  ),
+                  child: MinutesPicker(),
                 ),
               ],
             )),
           ],
         ),
       );
+}
+
+class MinutesPicker extends StatelessWidget {
+  const MinutesPicker({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NumberPicker.builder(
+      min: 0,
+      max: 60,
+      step: 5,
+      buildChild: (number) => Text(number.toString(),
+          style: Theme.of(context).textTheme.subtitle1),
+    );
+  }
+}
+
+class HoursPicker extends StatelessWidget {
+  const HoursPicker({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NumberPicker.builder(
+      min: 0,
+      max: 12,
+      step: 1,
+      buildChild: (number) => Text(number.toString(),
+          style: Theme.of(context).textTheme.subtitle1),
+    );
+  }
+}
+
+class PickerSeparator extends StatelessWidget {
+  const PickerSeparator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(":",
+        textAlign: TextAlign.center,
+        style: Theme.of(context)
+            .textTheme
+            .subtitle1
+            ?.copyWith(color: const Color(0x99ffffff)));
+  }
+}
+
+class PickerLabel extends StatelessWidget {
+  final String text;
+
+  const PickerLabel(
+    this.text, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: Theme.of(context)
+            .textTheme
+            .subtitle1
+            ?.copyWith(color: const Color(0x99ffffff)));
+  }
 }
